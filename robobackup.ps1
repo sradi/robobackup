3ad850cb28e027a888ff92a4db2db860f1229583
Stefan Rademacher <sradi@web.de>
2015-12-26 13:57:23 +0100

    + robocopy /NP: no progress info. Avoid bloating logfiles

--- robobackup.ps1
+++ robobackup.ps1
@@ -16,7 +16,7 @@ function startBackupJob($Name, $Source, $Target, $ExcludedFiles, $ExcludedDirect
 	$ErrFile = "$($LogBasePath)\robobackup_$($Name).err"
 	$TargetDir = "$($Target)\$Name" -replace "\\\\", "\"
 	$RobocopyOptions = @"
-$Source $TargetDir /E /MON:1 /MOT:$SyncInterval /ZB /FFT /R:100 /W:30 /XF $ExcludedFiles /XD $ExcludedDirectories
+$Source $TargetDir /E /MON:1 /MOT:$SyncInterval /ZB /FFT /NP /R:100 /W:30 /XF $ExcludedFiles /XD $ExcludedDirectories
 "@
 	$Proc = Start-Process -FilePath robocopy.exe -ArgumentList $RobocopyOptions -NoNewWindow -PassThru -RedirectStandardOutput $OutFile -RedirectStandardError $ErrFile
 	$Proc | Add-Member -MemberType NoteProperty -Name JobName -Value $Name
